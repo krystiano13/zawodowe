6c455d33cecadb84b61184198001434bfeacbcfe
krystiano13 <krystiano13@interklasa.pl>
2020-06-11 19:38:33 +0200

    Updates

---
+++ precache-manifest.4d967940641f7057710393c9fd71083a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c726bc99741ccb7b436a161491e4501",
+    "url": "/zawodowe/index.html"
+  },
+  {
+    "revision": "4c669faa6c25fa8e5f21",
+    "url": "/zawodowe/static/css/main.0db0159a.chunk.css"
+  },
+  {
+    "revision": "22fb6af1f097d64038a8",
+    "url": "/zawodowe/static/js/2.80191e44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zawodowe/static/js/2.80191e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c669faa6c25fa8e5f21",
+    "url": "/zawodowe/static/js/main.77ba95d8.chunk.js"
+  },
+  {
+    "revision": "48ff4e7c813445f0de7a",
+    "url": "/zawodowe/static/js/runtime-main.782ef5a8.js"
+  }
+]);
